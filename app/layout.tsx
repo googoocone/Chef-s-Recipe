09c5238698821d4c2915343f9c36e95c48e11408
googoocone1 <googoocone1@naver.com>
2025-12-31 10:49:01 +0900

    링크 미리보기 이미지 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,24 @@ import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://chef-s-recipe.vercel.app'),
   title: "Chef's Recipe - Premium Recipe Platform",
   description: 'Float into the world of flavor.',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: "Chef's Recipe",
+    description: 'Find the best recipes from top chefs.',
+    images: [
+      {
+        url: '/favicon.ico',
+        width: 1200,
+        height: 630,
+        alt: "Chef's Recipe Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
